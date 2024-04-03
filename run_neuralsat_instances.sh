b26593366799056af5da3d961d672f1f48f82a8f
Scott Sikorski <sgs.sikorski20@gmail.com>
2024-04-03 11:43:03 -0400

    More

---
+++ run_neuralsat_instances.sh
@@ -0,0 +1,27 @@
+cd neuralsat
+cd neuralsat-pt201
+#conda deactivate
+#conda activate neuralsat
+
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_0_eps_0.00784_n1.vnnlib
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_1_eps_0.00784_n1.vnnlib
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_2_eps_0.00784_n1.vnnlib
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_3_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_4_eps_0.00784_n1.vnnlib
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_5_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_6_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_8_eps_0.00784_n1.vnnlib
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_9_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_10_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_11_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_12_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_13_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_14_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_15_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_16_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_17_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_18_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_19_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_20_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_21_eps_0.00784_n1.vnnlib 
+python3 main.py --net ../neuralbench/instances/cifar2020/nnet/cifar10_2_255_simplified.onnx --spec ../neuralbench/instances/cifar2020/spec/cifar10_spec_idx_23_eps_0.00784_n1.vnnlib 
